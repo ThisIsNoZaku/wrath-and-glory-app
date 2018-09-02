ca4b696d88fa8522f3cc72141a1ac0dd7a77a45a
Damie <damienmarble@gmail.com>
2018-09-02 00:15:57 -0400

    Added Redux.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import RootReducer from "./reducers/Root";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reactStore = createStore(RootReducer);
+
+ReactDOM.render(<Provider store={reactStore}>
+    <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
