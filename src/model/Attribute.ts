99a7d07f2ef75db402e4486dfca54498a391c296
Damie <damienmarble@gmail.com>
2018-09-02 01:26:32 -0400

    Added type for Attributes.

---
+++ src/model/Attribute.ts
@@ -0,0 +1,8 @@
+type Attribute = "Strength" | "Agility" | "Toughness" | "Intellect" | "Willpower" | "Fellowship" | "Initiative";
+export const Strength:Attribute = "Strength";
+export const Agility:Attribute = "Agility";
+export const Toughness:Attribute = "Toughness";
+export const Intellect:Attribute = "Intellect";
+export const Willpower:Attribute = "Willpower";
+export const Fellowship:Attribute = "Fellowship";
+export const Initiative:Attribute = "Initiative";
